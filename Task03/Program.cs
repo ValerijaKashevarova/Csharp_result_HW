19ca83ebe234d3261472242c97a36a5a941d5ef7
Valeriia Maksimivskikh <kashevarovava@gmail.com>
2024-05-28 01:07:01 +0500

    Записала решени 3-го задания

---
+++ Task03/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+void Main()
+{
+    int[] tempMatrix = {2, 4};
+    CrossPrintArray(tempMatrix, 0);
+}
+
+Main();
+
+void CrossPrintArray(int[] array, int i)
+{
+    if (i >= array.Length) return;
+    CrossPrintArray(array, i++);
+    System.Console.WriteLine(array[i] + "");
+}
