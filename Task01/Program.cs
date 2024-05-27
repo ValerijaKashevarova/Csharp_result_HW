f07860107d95b0b3a87bdf14bf6feca5a70b1779
Valeriia Maksimivskikh <kashevarovava@gmail.com>
2024-05-28 00:54:53 +0500

    Добавила решение Задачи 1

---
+++ Task01/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+void Main()
+{
+    int M = ReadInt("Введите значение М: ");
+    int N = ReadInt("Введите значение N, которо должно быть больше M: ");
+    if (N > M)
+        PrintNumbers(M, N);
+    else
+        System.Console.WriteLine("Введен некорректный диапазон, вычисления невозможны!");
+    
+}
+
+Main();
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumbers(int M, int N)
+{
+    if(N < M) return;
+    PrintNumbers(M, N - 1);
+    System.Console.Write(N + " ");
+}
